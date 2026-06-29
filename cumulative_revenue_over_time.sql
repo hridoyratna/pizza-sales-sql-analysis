ea769ea7c6fd2f952d2ad8bd55a934b9735261f0
Hridoy Ratna <hridoyratna09@gmail.com>
2026-06-30 01:05:02 +0600

    Add SQL analysis files and datasets

---
+++ cumulative_revenue_over_time.sql
@@ -0,0 +1,9 @@
+-- Analyze the cumulative revenue generated over time.
+
+SELECT order_date,SUM(revenue) OVER(ORDER BY order_date) AS cumulative_revenue
+FROM (SELECT orders.order_date, ROUND(SUM(order_details.quantity * pizzas.price),2) AS revenue
+FROM order_details JOIN pizzas
+ON order_details.pizza_id = pizzas.pizza_id
+JOIN orders 
+ON orders.order_id = order_details.order_id
+GROUP BY orders.order_date) AS sales;
